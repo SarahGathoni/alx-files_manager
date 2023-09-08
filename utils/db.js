c032bd1487a0dccec06d77b4e01d47fd1f7f3b21
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-09-08 08:45:30 -0700

    updates

--- utils/db.js
+++ utils/db.js
@@ -1,74 +1,37 @@
-**
- * MongoDB client connection class
- */
-import { MongoClient, ObjectId } from 'mongodb';
+import { MongoClient } from 'mongodb';
+
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
-    const DB_PORT = process.env.DB_PORT || '27017';
-    const DB_HOST = process.env.DB_HOST || 'localhost';
-    const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-    const DB_URL = `mongodb://${DB_HOST}:${DB_PORT}`;
-    this._client = new MongoClient(DB_URL);
-    this.connected = false;
-    (async () => {
-      try {
-        await this._client.connect();
-        this.connected = true;
-        this._db = this._client.db(DB_DATABASE);
-        await this._db.collection('users').createIndex({ email: 1 }, { unique: true });
-      } catch (error) {
-        console.log(error);
-      }
-    })();
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   isAlive() {
-    return this.connected;
+    return this.client.isConnected();
   }
 
-  async nbUsers(query = {}) {
-    return this._db.collection('users').countDocuments(query);
+  async nbUsers() {
+    const users = this.db.collection('users');
+    const usersNum = await users.countDocuments();
+    return usersNum;
   }
 
-  async nbFiles(query = {}) {
-    return this._db.collection('files').countDocuments(query);
-  }
-
-  async insertOne(collection, data) {
-    return this._db.collection(collection).insertOne(data);
-  }
-
-  async findOne(collection, data) {
-    return this._db.collection(collection).findOne(data);
-  }
-
-  async updateOne(collection, filter, updateDoc) {
-    return this._db.collection(collection).updateOne(filter, updateDoc);
-  }
-
-  async listFiles(data) {
-    let pipeline = [];
-    const { userId, page } = data;
-    let { parentId } = data;
-    if (parentId === undefined) {
-      pipeline = [
-        { $match: { userId: ObjectId(userId) } },
-        { $facet: { data: [{ $skip: page * 20 }, { $limit: 20 }] } },
-      ];
-    } else {
-      if (parentId !== '0') {
-        parentId = ObjectId(parentId);
-      }
-      pipeline = [
-        { $match: { userId: ObjectId(userId), parentId } },
-        { $facet: { data: [{ $skip: page * 20 }, { $limit: 20 }] } },
-      ];
-    }
-    return this._db.collection('files').aggregate(pipeline).toArray();
+  async nbFiles() {
+    const files = this.db.collection('files');
+    const filesNum = await files.countDocuments();
+    return filesNum;
   }
 }
 
 const dbClient = new DBClient();
-
 module.exports = dbClient;
